2602d5362c972df6cd81c4ba9029b7d7ba8da578
amey-kamde <ameykamde999@gmail.com>
2026-04-06 15:55:48 +0530

    first commit

---
+++ Extent_Report/demo/src/test/java/hooks/Hooks.java
@@ -0,0 +1,25 @@
+package hooks;
+
+import io.cucumber.java.Before;
+import io.cucumber.java.After;
+import utils.DriverFactory;
+import utils.ExtentManager;
+import com.aventstack.extentreports.*;
+
+public class Hooks {
+
+    static ExtentReports extent = ExtentManager.getInstance();
+    static ExtentTest test;
+
+    @Before
+    public void setup(io.cucumber.java.Scenario scenario) {
+        test = extent.createTest(scenario.getName());
+        DriverFactory.initDriver();
+    }
+
+    @After
+    public void tearDown() {
+        // DriverFactory.quitDriver();
+        extent.flush();
+    }
+}
